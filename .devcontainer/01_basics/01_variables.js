f3bb261e993012a765d4bd76a24453b3c43022d3
Ansh Sharma <sharmaansh6717@gmail.com>
2026-08-12 09:25:16 +0000

    datatypes

---
+++ .devcontainer/01_basics/01_variables.js
@@ -0,0 +1,4 @@
+const accountId = 34353
+let accountUsername = "ansh12345"
+var password = "23444"
+console.table([accountId,accountUsername,password])
---
+++ .devcontainer/01_basics/02_datatypes.js
@@ -0,0 +1,15 @@
+"use strict"//treat all JS code as newer version
+//datatypes
+//number -> 2 to the power 53 range
+//bigint
+//string => ""
+//boolean 
+//null => standalone value 
+//undedfined 
+//symbol => unique 
+
+
+//object
+console.log(typeof 18)
+
+console.log(typeof "ansh")
