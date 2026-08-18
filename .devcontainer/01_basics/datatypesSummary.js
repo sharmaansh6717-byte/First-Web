b813ae01a96db7127f1f49565ccaf20a5a04574c
Ansh Sharma <sharmaansh6717@gmail.com>
2026-08-18 16:50:21 +0000

    stack and heap memory

--- .devcontainer/01_basics/datatypesSummary.js
+++ .devcontainer/01_basics/datatypesSummary.js
@@ -52,3 +52,24 @@ console.log(typeof anotherId);
 //        Function  =>  function
 //        Object  =>  object
 
+//+++++++++++++++++++++++++++++++++++++++++++++++++++++
+
+//Stack and Heap Memory in JavaScript
+
+// Stack Memory => Primitive Datatypes
+// Heap Memory => Non-Primitive Datatypes
+
+let myName = "Ansh" // stored in stack memory
+
+let myanotherName = myName // stored in stack memory
+
+let userOne = {
+    email: "sharmasjfk@gmail.com",    // stored in heap memoryƒ•
+    name: "user"
+}
+
+let userTwo = userOne // stored in heap memory  the userTwo is pointing to the same reference of userOne hence if i change the value of userTwo it will also change the value of userOne because they are pointing to the same reference in heap memory.
+
+userTwo.email = "anshsharma@gmail.com"
+
+console.log(userOne.email); // output => anshsharma@gmail.com
