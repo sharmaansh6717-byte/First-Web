4c4593e0df6fadb9b4d6942d4859244a0ee1f39f
Ansh Sharma <sharmaansh6717@gmail.com>
2026-09-11 09:58:09 +0000

    conditional stat

---
+++ .devcontainer/04_control_flow/one.js
@@ -0,0 +1,13 @@
+//control flow is just flow of logics of the code
+
+//if 
+
+//if(condition){}
+
+//shorthand foramt for if without using scope 
+
+const balance = 1000;
+if(balance > 500) console.log(`Enough balance of ${balance}`);
+
+
+
