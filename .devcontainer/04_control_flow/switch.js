52dc09777e91303eea8ff36ea1fec34b91318a2b
Ansh Sharma <sharmaansh6717@gmail.com>
2026-09-11 10:03:06 +0000

    switch

---
+++ .devcontainer/04_control_flow/switch.js
@@ -0,0 +1,10 @@
+switch (expression) {
+  case value1:
+    // code to execute if expression === value1
+    break;
+  case value2:
+    // code to execute if expression === value2
+    break;
+  default:
+    // code to execute if expression doesn't match any case
+}
