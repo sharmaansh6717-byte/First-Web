3df36bf436398074e78dbdb67e0669b0912a2840
Ansh Sharma <sharmaansh6717@gmail.com>
2026-09-14 18:22:44 +0000

    ..

--- .devcontainer/05_Iterations/one.js
+++ .devcontainer/05_Iterations/one.js
@@ -1,3 +1,15 @@
 for(let i = 0; i < 5; i++) {
     console.log("Iteration number: " + i);
 }   
+for(let i = 0; i < 5; i++) {
+    if(i === 3) {
+        break; // exit the loop when i is 3
+    }
+    console.log("Iteration number: " + i);
+}   
+for(let i = 0; i < 5; i++) {
+    if(i === 3) {
+        continue; // skip the rest of the loop when i is 3
+    }
+    console.log("Iteration number: " + i);
+}
